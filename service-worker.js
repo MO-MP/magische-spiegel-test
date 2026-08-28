965175f3d2437083d702ff89040a6ee95e174aa9
MO-MP <322112802+MO-MP@users.noreply.github.com>
2026-08-28 19:32:24 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,34 @@
-const CACHE_NAME = "magische-spiegel-test-v2";
+const CACHE_NAME = "magische-spiegel-test-v3";
 
 const BESTANDEN = [
-"./",
-"./index.html"
+  "./",
+  "./index.html"
 ];
 
 self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(BESTANDEN))
-);
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(BESTANDEN))
+  );
 });
 
 self.addEventListener("activate", event => {
-event.waitUntil(
-caches.keys().then(names =>
-Promise.all(
-names
-.filter(name => name !== CACHE_NAME)
-.map(name => caches.delete(name))
-)
-)
-);
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(
+        names
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => response || fetch(event.request))
-);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
